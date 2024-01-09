2d161a0e24fb31cac2eaeb13bd1d8a5e7906b45e
Kilian Boute <kilian.boute@gmail.com>
2024-01-09 10:29:43 +0100

    ex 7

--- OOP/exercise_7_static.php
+++ OOP/exercise_7_static.php
@@ -11,3 +11,43 @@ TODO: Print the address without creating a new instant of the beverage class 2 t
 
 Use typehinting everywhere!
 */
+
+require_once "exercise_1_classes.php";
+
+const barname = 'Het Vervolg';
+echo barname;
+
+class Beer extends Beverage
+{
+
+    private $name;
+    private $alcoholPercentage;
+
+    private static $adress = "Melkmarkt 9, 2000 Antwerpen";
+
+    public function __construct(string $color, float $price, string $name, float $alcoholPercentage)
+    {
+        parent::__construct($color, $price);
+
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+
+    public function getAlcoholPercentage()
+    {
+        echo "Alcoholpercentage: $this->alcoholPercentage% </br>";
+    }
+
+    public function getBarname()
+    {
+        echo barname;
+    }
+
+    public function getAdress()
+    {
+        echo $this->adress;
+    }
+}
+
+$Duvel = new Beer("blond", 3.5, "Duvel", 8.5);
+$Duvel->getBarname();
